3bb8a26b6223d705d541c284e42b759285c46174
Hunter Atkisson <s259081@students.aie.edu>
2025-09-29 12:02:29 -0700

    added colour and more  stuff

--- MathClasses tests/MathClasses tests.cpp
+++ MathClasses tests/MathClasses tests.cpp
@@ -640,7 +640,84 @@ public:
     }
 };
 
+TEST_CLASS(ColourTests)
+{
+public:
 
+    // Test 1: Default constructor
+    TEST_METHOD(DefaultConstructor)
+    {
+        Colour c;
+        Assert::AreEqual((int)c.getRed(), 0);
+        Assert::AreEqual((int)c.getGreen(), 0);
+        Assert::AreEqual((int)c.getBlue(), 0);
+        Assert::AreEqual((int)c.getAlpha(), 0);
+    }
 
+    // Test 2: Constructor with values
+    TEST_METHOD(ConstructorWithValues)
+    {
+        Colour c(255, 128, 64, 32);
+        Assert::AreEqual((int)c.getRed(), 255);
+        Assert::AreEqual((int)c.getGreen(), 128);
+        Assert::AreEqual((int)c.getBlue(), 64);
+        Assert::AreEqual((int)c.getAlpha(), 32);
+    }
 
+    // Test 3: Setters and Getters
+    TEST_METHOD(SettersAndGetters)
+    {
+        Colour c;
+        c.setRed(10);
+        c.setGreen(20);
+        c.setBlue(30);
+        c.setAlpha(40);
 
+        Assert::AreEqual((int)c.getRed(), 10);
+        Assert::AreEqual((int)c.getGreen(), 20);
+        Assert::AreEqual((int)c.getBlue(), 30);
+        Assert::AreEqual((int)c.getAlpha(), 40);
+    }
+
+    // Test 4: Store red = 94 (Q3–Q5 from exercise)
+    TEST_METHOD(Red94Value)
+    {
+        Colour c(94, 0, 0, 0);
+        Assert::AreEqual((int)c.getRed(), 94);
+
+        unsigned int expected = 94u << 24; // red in left-most byte
+        Assert::AreEqual(expected, c.getValue());
+    }
+
+    // Test 5: Shift red into green (Q6–Q7 from exercise)
+    TEST_METHOD(ShiftRedIntoGreen)
+    {
+        Colour c(94, 0, 0, 0);
+        unsigned int val = c.getValue();
+
+        // Move red into green
+        val = (val >> 8) & 0x00FF0000;
+        c.setValue(val);
+
+        Assert::AreEqual((int)c.getRed(), 0);
+        Assert::AreEqual((int)c.getGreen(), 94);
+        Assert::AreEqual((int)c.getBlue(), 0);
+        Assert::AreEqual((int)c.getAlpha(), 0);
+
+        unsigned int expected = 94u << 16;
+        Assert::AreEqual(expected, c.getValue());
+    }
+
+    // Test 6: Edge case (all max values)
+    TEST_METHOD(AllMaxValues)
+    {
+        Colour c(255, 255, 255, 255);
+        Assert::AreEqual((int)c.getRed(), 255);
+        Assert::AreEqual((int)c.getGreen(), 255);
+        Assert::AreEqual((int)c.getBlue(), 255);
+        Assert::AreEqual((int)c.getAlpha(), 255);
+
+        unsigned int expected = 0xFFFFFFFF;
+        Assert::AreEqual(expected, c.getValue());
+    }
+};
